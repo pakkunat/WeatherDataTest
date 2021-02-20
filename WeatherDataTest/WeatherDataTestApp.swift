de6a84a9dd9653bba86d04c3856328cc48b10f0a
pakkun <pakkun_kh@fol.hi-ho.ne.jp>
2021-02-21 08:53:56 +0900

    Initial Commit

---
+++ WeatherDataTest/WeatherDataTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDataTestApp.swift
+//  WeatherDataTest
+//
+//  Created by pakkun on 2021/02/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDataTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
