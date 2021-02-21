e2f24227688540c9a0671aa76449b5f514b0d3a5
pakkun <pakkun_kh@fol.hi-ho.ne.jp>
2021-02-21 11:32:23 +0900

    .

--- WeatherDataTest/ContentView.swift
+++ WeatherDataTest/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State var tempData: String = ""
+  
+  var body: some View {
+    VStack {
+      Button("start", action: start)
+      TextEditor(text: $tempData)
     }
+  }
+  
+  func start() {
+    let url = URL(string: "https://www.data.jma.go.jp/obd/stats/data/mdrr/tem_rct/alltable/mxtemsadext00_rct.csv")!
+    let downloadTask = URLSession.shared.downloadTask(with: url) {urlOrNil, responseOrNil, errorOrNil in
+      guard let fileURL = urlOrNil else { return }
+      do {
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+        let savedURL = documentsURL?.appendingPathComponent(fileURL.lastPathComponent)
+        try FileManager.default.moveItem(at: fileURL, to: savedURL!)
+        let csvString = try String(contentsOf: savedURL!, encoding: String.Encoding.shiftJIS)
+        let csvLines = csvString.components(separatedBy: .newlines)
+        let dateTime = csvLines[2].components(separatedBy: ",")
+        tempData = "\(dateTime[4])/\(dateTime[5])/\(dateTime[6]) 広島県 最高気温\n\n"
+        for element in csvLines {
+          if element == "" { continue }
+          let data = element.components(separatedBy: ",")
+          if data[1] == "広島県" {
+            tempData += "\(data[2]): \(data[9]) ℃\n"
+          }
+        }
+      } catch {
+      }
+    }
+    downloadTask.resume()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
--- WeatherDataTest/WeatherDataTestApp.swift
+++ WeatherDataTest/WeatherDataTestApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct WeatherDataTestApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
